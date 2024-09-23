2e52c2c348d80e8d87217c70cd7fd2d8244a2293
Mathprimus <mathprimus@gmail.com>
2024-09-23 17:39:27 -0300

    Exercicio3

---
+++ exercicio002.py
@@ -0,0 +1,3 @@
+nome = input('Qual o seu nome?')
+print('Ola {} seja bem vindo!'.format(nome))
+print('É um prazer em te conhecer {}!'. format(nome))
---
+++ exercicio003.py
@@ -0,0 +1,5 @@
+valor1 = int(input('Digite o valor1: '))
+valor2 = int(input('Digite o valor2: '))
+soma = valor1 + valor2
+
+print(soma)
